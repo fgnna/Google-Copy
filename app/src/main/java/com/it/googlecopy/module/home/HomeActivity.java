be1a2f4cdc87827fe3fadd5683d304cd7c1802bf
sjy1991 <sjy199110@gmail.com>
2016-10-04 09:10:17 +0800

    新增导航fragment

--- app/src/main/java/com/it/googlecopy/module/home/HomeActivity.java
+++ app/src/main/java/com/it/googlecopy/module/home/HomeActivity.java
@@ -1,99 +1,80 @@
 package com.it.googlecopy.module.home;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.it.googlecopy.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
 
-    @BindView(R.id.m_home_firstpager)
-    RelativeLayout mMHomeFirstpager;
-    @BindView(R.id.m_home_collection)
-    RelativeLayout mMHomeCollection;
-    @BindView(R.id.m_home_work)
-    RelativeLayout mMHomeWork;
-    @BindView(R.id.m_home_notifications)
-    RelativeLayout mMHomeNotifications;
-    @BindView(R.id.fragments)
-    FrameLayout mFragments;
-    @BindView(R.id.m_home_drawleft_personaldata)
-    RelativeLayout mMHomeDrawleftPersonaldata;
-    @BindView(R.id.m_home_drawleft_connection)
-    RelativeLayout mMHomeDrawleftConnection;
-    @BindView(R.id.m_home_drawleft_location)
-    RelativeLayout mMHomeDrawleftLocation;
-    @BindView(R.id.m_home_drawleft_activity)
-    RelativeLayout mMHomeDrawleftActivity;
-    @BindView(R.id.m_home_drawleft_setting)
-    RelativeLayout mMHomeDrawleftSetting;
-    @BindView(R.id.m_home_drawleft_send)
-    RelativeLayout mMHomeDrawleftSend;
-    @BindView(R.id.m_home_drawleft_help)
-    RelativeLayout mMHomeDrawleftHelp;
-    private Toolbar mToolbar;
+    public Toolbar mToolbar;
     FragmentManager fm;
-    Home_Fragment home_fragment;
-    Collection_Fragment collection_fragment;
-    Work_Fragment work_fragment;
-    Notifications_Fragment notifications_fragment;
+    HomeFragment home_fragment;
+    CollectionFragment collection_fragment;
+    WorkFragment work_fragment;
+    NotificationsFragment notifications_fragment;
+    RelativeLayout mFirstPager;
+    RelativeLayout mCollection;
+    RelativeLayout mWork;
+    RelativeLayout mNotifications;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.home_main);
-        ButterKnife.bind(this);
+
+
         initView();
-        initToolbar();
+        initToolbar("首页");
         initDrawerLayout();
         initFragments();
         initListener();
 
+
     }
 
     private void initListener() {
-
+        mFirstPager.setOnClickListener(this);
+        mCollection.setOnClickListener(this);
+        mWork.setOnClickListener(this);
+        mNotifications.setOnClickListener(this);
     }
 
     private void initFragments() {
-        home_fragment = new Home_Fragment();
-        Bundle bundle = new Bundle();
-        collection_fragment = new Collection_Fragment();
-        work_fragment = new Work_Fragment();
-        notifications_fragment = new Notifications_Fragment();
+        home_fragment = new HomeFragment();
+        collection_fragment = new CollectionFragment();
+        work_fragment = new WorkFragment();
+        notifications_fragment = new NotificationsFragment();
+
     }
 
-    private void initDrawerLayout() {
+    public void initDrawerLayout() {
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
                 drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string
                 .navigation_drawer_close);
         drawerToggle.syncState();
-        LinearLayout drawLeft = (LinearLayout) findViewById(R.id.m_home_drawleft);
+
 
     }
 
-    private void initToolbar() {
+    public void initToolbar(String title) {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle("首页");
+        mToolbar.setTitle(title);
         setSupportActionBar(mToolbar);
     }
 
@@ -102,10 +83,10 @@ public class HomeActivity extends AppCompatActivity {
         CircleImageView mHomeDrawLayoutHead = (CircleImageView) findViewById(R.id
                 .m_home_drawleft_head);
 
-        /*ImageView ivtest = (ImageView) findViewById(R.id.test);
-        Glide.with(this).load("http://img.woyaogexing.com/2016/09/30/70d7aa164c0cc43f!200x200" +
-                ".jpg").placeholder(R.drawable.ic_help_black_24dp).error(R.drawable
-                .ic_account_circle_black_24dp).into(mHomeDrawLayoutHead);*/
+        mFirstPager = (RelativeLayout) findViewById(R.id.m_home_firstpager);
+        mCollection = (RelativeLayout) findViewById(R.id.m_home_collection);
+        mWork = (RelativeLayout) findViewById(R.id.m_home_work);
+        mNotifications = (RelativeLayout) findViewById(R.id.m_home_notifications);
 
         Glide.with(this).load(R.drawable.bg).into(mHomeDrawLayoutHead);
 
@@ -113,26 +94,29 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.m_home_firstpager, R.id.m_home_collection, R.id.m_home_work, R.id
-            .m_home_notifications,R.id.m_home_drawleft_personaldata, R.id.m_home_drawleft_connection, R.id
-            .m_home_drawleft_location, R.id.m_home_drawleft_activity, R.id
-            .m_home_drawleft_setting, R.id.m_home_drawleft_send, R.id.m_home_drawleft_help})
+
     public void onClick(View view) {
         fm = getSupportFragmentManager();
         switch (view.getId()) {
             case R.id.m_home_firstpager:
-                System.out.println("first");
-                fm.beginTransaction().replace(R.id.fragments, home_fragment).commit();
+
+                if(findViewById(R.id.empty_fragment) != null) {
+                    System.out.println("first");
+                    //fm.beginTransaction().replace(R.id.empty_fragment, home_fragment).commit();
+                    fm.beginTransaction().show(home_fragment).commit();
+                }
                 break;
             case R.id.m_home_collection:
                 System.out.println("collection");
-                fm.beginTransaction().replace(R.id.fragments, collection_fragment).commit();
+                fm.beginTransaction().replace(R.id.empty_fragment, collection_fragment).commit();
                 break;
             case R.id.m_home_work:
-                fm.beginTransaction().replace(R.id.fragments, work_fragment).commit();
+                System.out.println("work");
+                fm.beginTransaction().replace(R.id.empty_fragment, work_fragment).commit();
                 break;
             case R.id.m_home_notifications:
-                fm.beginTransaction().replace(R.id.fragments, notifications_fragment).commit();
+                System.out.println("notifications");
+                fm.beginTransaction().replace(R.id.empty_fragment, notifications_fragment).commit();
                 break;
 
             case R.id.m_home_drawleft_personaldata:
---
+++ app/src/main/java/com/it/googlecopy/module/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.it.googlecopy.module.home;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.it.googlecopy.R;
+import com.it.googlecopy.base.BaseFragment;
+
+/**
+ * Created by je on 16-10-2.
+ */
+
+public class HomeFragment extends Fragment {
+    Toolbar mToolbar;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container,true);
+        mToolbar= (Toolbar) view.findViewById(R.id.toolbar);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        HomeActivity activity = (HomeActivity) getActivity();
+        mToolbar.setTitle("首页");
+        activity.setSupportActionBar(mToolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(activity,
+                drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string
+                .navigation_drawer_close);
+        drawerToggle.syncState();
+
+    }
+}
